6670b4d00f5afc9eaf681a32f33a77a430e4d923
sunepoulsen <sunepoulsen@gmail.com>
2020-08-21 23:20:58 +0200

    TEL-11: Test of Spring Security with Auth 2.0

--- tel-authentication-backend-service/src/main/java/dk/sunepoulsen/tech/enterprise/labs/authentication/service/Application.java
+++ tel-authentication-backend-service/src/main/java/dk/sunepoulsen/tech/enterprise/labs/authentication/service/Application.java
@@ -3,12 +3,14 @@ package dk.sunepoulsen.tech.enterprise.labs.authentication.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static dk.sunepoulsen.tech.enterprise.labs.core.service.utils.SpringBootApplicationUtils.COMPONENT_SCAN_PACKAGES;
 
 @EnableWebMvc
 @EnableAsync
+@EnableAuthorizationServer
 @SpringBootApplication( scanBasePackages = { COMPONENT_SCAN_PACKAGES } )
 public class Application {
     public static void main( String[] args ) {
